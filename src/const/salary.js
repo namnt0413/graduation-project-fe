0f87797b28dabfc51a219892bf6fd55813a8c653
namnt0413 <namnt0413@gmail.com>
2024-06-10 18:40:45 +0700

    feat: recommend jobs

---
+++ src/const/salary.js
@@ -0,0 +1,10 @@
+export const Salary = [
+    'dưới 5 triệu',
+    '5-10 triệu',
+    '10-15 triệu',
+    '15-20 triệu',
+    '20-25 triệu',
+    '25-30 triệu',
+    '30-50 triệu',
+    'trên 50 triệu',
+]
