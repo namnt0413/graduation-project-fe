042919c030f6b389b1ee586ea49098394794b07d
namnt0413 <namnt0413@gmail.com>
2024-06-11 18:47:22 +0700

    update recommend jobs

--- src/containers/HomeMainContent/HomeLeftContent/TopCompany.js
+++ src/containers/HomeMainContent/HomeLeftContent/TopCompany.js
@@ -42,74 +42,6 @@ const TopCompany = () => {
           </div>
           <div className="jp_hiring_slider_wrapper top-campany-list">
             <Slider {...settings}>
-              <div className="company-item">
-                <div className="jp_hiring_content_main_wrapper">
-                  <div className="jp_hiring_content_wrapper">
-                    <img
-                      src="images/content/hiring_img1.png"
-                      alt="hiring_img"
-                    />
-                    <h4>Akshay INC.</h4>
-                    <p>(NewYork)</p>
-                    <ul>
-                      <li>
-                        <a href="#">4 Opening</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="company-item">
-                <div className="jp_hiring_content_main_wrapper">
-                  <div className="jp_hiring_content_wrapper">
-                    <img
-                      src="images/content/hiring_img2.png"
-                      alt="hiring_img"
-                    />
-                    <h4>Akshay INC.</h4>
-                    <p>(NewYork)</p>
-                    <ul>
-                      <li>
-                        <a href="#">4 Opening</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="company-item">
-                <div className="jp_hiring_content_main_wrapper">
-                  <div className="jp_hiring_content_wrapper">
-                    <img
-                      src="images/content/hiring_img3.png"
-                      alt="hiring_img"
-                    />
-                    <h4>Akshay INC.</h4>
-                    <p>(NewYork)</p>
-                    <ul>
-                      <li>
-                        <a href="#">4 Opening</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="company-item">
-                <div className="jp_hiring_content_main_wrapper">
-                  <div className="jp_hiring_content_wrapper">
-                    <img
-                      src="images/content/hiring_img4.png"
-                      alt="hiring_img"
-                    />
-                    <h4>Akshay INC.</h4>
-                    <p>(NewYork)</p>
-                    <ul>
-                      <li>
-                        <a href="#">4 Opening</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
               {companies.map((company, index) => (
                 <div className="company-item">
                   <div
@@ -126,7 +58,7 @@ const TopCompany = () => {
                         className="company-avatar"
                       />
                       <h4>{company.name}</h4>
-                      <p>{company.address}</p>
+                      <p>{company.city.name}</p>
                       <ul>
                         <li>
                           <a href="#">4 Jobs Opening</a>
--- src/pages/JobList.js
+++ src/pages/JobList.js
@@ -57,18 +57,34 @@ const JobList = () => {
   const convertedCategories = convertCategories(categories);
 
   const handleRecomendJobs = () => {
-    const categoryList = category.map((item) => item.value).join(",");
+    const categoryList = category
+      ? category.map((item) => item.value).join(",")
+      : undefined;
+    console.log(city, exp, salary, categoryList);
+
     const getRecommendJobs = async () => {
-      const res = await axios.get("/api/job/recommend-jobs", {
-        params: {
-          exp: exp,
-          city: city,
-          salary: salary,
-          category: categoryList,
-        },
-      });
+      const params = {};
+
+      if (exp !== undefined && exp !== "") {
+        params.exp = exp;
+      }
+
+      if (city !== undefined && city !== "") {
+        params.city = city;
+      }
+
+      if (salary !== undefined && salary !== "") {
+        params.salary = salary;
+      }
+
+      if (categoryList !== undefined && categoryList !== "") {
+        params.category = categoryList;
+      }
+
+      const res = await axios.get("/api/job/recommend-jobs", { params });
       setRecommendJobs(res.data.recommendJobs);
     };
+
     getRecommendJobs();
   };
 
@@ -78,80 +94,267 @@ const JobList = () => {
 
   const JobListContent = (
     <>
-      <div className="jp_listing_sidebar_main_wrapper">
-        <div className="container">
+      <div className="jp_listing_sidebar_main_wrapper container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 hidden-sm hidden-xs">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div class="jp_rightside_job_categories_wrapper jp_job_location_wrapper">
+                <div class="jp_rightside_job_categories_heading">
+                  <h4>Cpmpanies by Location</h4>
+                </div>
+                <div class="jp_rightside_job_categories_content">
+                  <div class="handyman_sec1_wrapper">
+                    <div class="content">
+                      <div class="box">
+                        <p>
+                          <input type="checkbox" id="c9" name="cb" />
+                          <label for="c9">
+                            Jobs in Delhi <span>(214)</span>
+                          </label>
+                        </p>
+
+                        <p>
+                          <input type="checkbox" id="c10" name="cb" />
+                          <label for="c10">
+                            Jobs in Mumbai <span>(514)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c11" name="cb" />
+                          <label for="c11">
+                            Jobs in Chennai <span>(554)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c12" name="cb" />
+                          <label for="c12">
+                            Jobs in Gurgaon <span>(457)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c13" name="cb" />
+                          <label for="c13">
+                            Jobs in Noida <span>(1254)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c14" name="cb" />
+                          <label for="c14">
+                            Jobs in Kolkata <span>(554)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c15" name="cb" />
+                          <label for="c15">
+                            Jobs in Hyderabad <span>(350)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c16" name="cb" />
+                          <label for="c16">
+                            Jobs in Pune <span>(221)</span>
+                          </label>
+                        </p>
+                      </div>
+                    </div>
+                    <ul>
+                      <li>
+                        <i class="fa fa-plus-circle"></i>{" "}
+                        <a href="#">SHOW MORE</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div class="jp_rightside_job_categories_wrapper jp_job_location_wrapper">
+                <div class="jp_rightside_job_categories_heading">
+                  <h4>Your Skill’s</h4>
+                </div>
+                <div class="jp_rightside_job_categories_content">
+                  <div class="handyman_sec1_wrapper">
+                    <div class="content">
+                      <div class="box">
+                        <p>
+                          <input type="checkbox" id="c17" name="cb" />
+                          <label for="c17">
+                            Javascript <span>(214)</span>
+                          </label>
+                        </p>
+
+                        <p>
+                          <input type="checkbox" id="c18" name="cb" />
+                          <label for="c18">
+                            HTML5 <span>(514)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c19" name="cb" />
+                          <label for="c19">
+                            CSS3 <span>(554)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c20" name="cb" />
+                          <label for="c20">
+                            PHP <span>(457)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c21" name="cb" />
+                          <label for="c21">
+                            Sales <span>(1254)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c22" name="cb" />
+                          <label for="c22">
+                            Marketing <span>(554)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c23" name="cb" />
+                          <label for="c23">
+                            Social Media <span>(350)</span>
+                          </label>
+                        </p>
+                        <p>
+                          <input type="checkbox" id="c24" name="cb" />
+                          <label for="c24">
+                            Web Design <span>(221)</span>
+                          </label>
+                        </p>
+                      </div>
+                    </div>
+                    <ul>
+                      <li>
+                        <i class="fa fa-plus-circle"></i>{" "}
+                        <a href="#">SHOW MORE</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div class="jp_add_resume_wrapper jp_job_location_wrapper">
+                <div class="jp_add_resume_img_overlay"></div>
+                <div class="jp_add_resume_cont">
+                  <img src="images/content/resume_logo.png" alt="logo" />
+                  <h4>Get Best Matched Jobs On your Email. Add Resume NOW!</h4>
+                  <ul>
+                    <li>
+                      <a href="#">
+                        <i class="fa fa-plus-circle"></i> &nbsp;ADD RESUME
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-9 col-md-9">
           <div className="row">
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div className="jp_listing_heading_wrapper">
-                <h2>Danh sach cong viec phu hop</h2>
+              <div className="jp_listing_tabs_wrapper list-tabs">
+                <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                  <div className="gc_causes_view_tabs_wrapper">
+                    <div className="gc_causes_view_tabs">
+                      <ul className="nav nav-pills">
+                        <li className="active">
+                          <a data-toggle="pill" href="#grid">
+                            <i className="fa fa-th-large"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a data-toggle="pill" href="#list">
+                            <i className="fa fa-list"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                  <div className="gc_causes_search_box_wrapper gc_causes_search_box_wrapper2">
+                    <div className="gc_causes_search_box jobs-header">
+                      Danh sách công việc HOT
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <div className="row">
                 <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div className="col-lg-2 col-md-3 col-sm-12 col-xs-12">
-                      <div className="jp_form_location_wrapper">
-                        <i className="fa fa-dot-circle-o first_icon"></i>
-                        <select
-                          onChange={(e) => {
-                            setCity(e.target.value);
-                          }}
-                        >
-                          <option value={null}>Địa điểm</option>
-                          {cities.map((city, index) => (
-                            <option key={index} value={city.id || ""}>
-                              {city.name}
-                            </option>
-                          ))}
-                        </select>
-                        <i className="fa fa-angle-down second_icon"></i>
+                  {/* filter bar */}
+                  <div className="filtar-bar col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div className="col-lg-10  col-md-10 col-sm-12 col-xs-12">
+                      <div className="col-lg-4 col-md-3 col-sm-12 col-xs-12">
+                        <div className="jp_form_location_wrapper">
+                          <i className="fa fa-dot-circle-o first_icon"></i>
+                          <select
+                            onChange={(e) => {
+                              setCity(e.target.value);
+                            }}
+                          >
+                            <option value="">Địa điểm</option>
+                            {cities.map((city, index) => (
+                              <option key={index} value={city.id || ""}>
+                                {city.name}
+                              </option>
+                            ))}
+                          </select>
+                          <i className="fa fa-angle-down second_icon"></i>
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-lg-2 col-md-3 col-sm-12 col-xs-12">
-                      <div className="jp_form_exper_wrapper">
-                        <Select
-                          isMulti
-                          options={convertedCategories}
-                          value={category}
-                          onChange={setCategory}
-                        />
+                      <div className="col-lg-8 col-md-3 col-sm-12 col-xs-12">
+                        <div className="jp_form_exper_wrapper">
+                          <Select
+                            isMulti
+                            options={convertedCategories}
+                            value={category}
+                            onChange={setCategory}
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-lg-2 col-md-3 col-sm-12 col-xs-12">
-                      <div className="jp_form_location_wrapper">
-                        <i className="fa fa-dot-circle-o first_icon"></i>
-                        <select
-                          onChange={(e) => {
-                            setExp(e.target.value);
-                          }}
-                        >
-                          <option value={null}>Kinh nghiệm</option>
-                          {exps.map((exp, index) => (
-                            <option key={index} value={exp.id || ""}>
-                              {exp.name}
-                            </option>
-                          ))}
-                        </select>
-                        <i className="fa fa-angle-down second_icon"></i>
+                      <div className="col-lg-6 col-md-3 col-sm-12 col-xs-12">
+                        <div className="jp_form_location_wrapper">
+                          <i className="fa fa-dot-circle-o first_icon"></i>
+                          <select
+                            onChange={(e) => {
+                              setExp(e.target.value);
+                            }}
+                          >
+                            <option value="">Kinh nghiệm</option>
+                            {exps.map((exp, index) => (
+                              <option key={index} value={exp.id || ""}>
+                                {exp.name}
+                              </option>
+                            ))}
+                          </select>
+                          <i className="fa fa-angle-down second_icon"></i>
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                      <div className="jp_form_exper_wrapper">
-                        <i className="fa fa-dot-circle-o first_icon"></i>
-                        <select
-                          onChange={(e) => {
-                            setSalary(e.target.value);
-                          }}
-                        >
-                          <option value={null}>Mức lương</option>
-                          {Salary.map((salary, index) => (
-                            <option key={index + 1} value={index + 1 || ""}>
-                              {salary}
-                            </option>
-                          ))}
-                        </select>
-                        <i className="fa fa-angle-down second_icon"></i>
+                      <div className="col-lg-6 col-md-3 col-sm-12 col-xs-12">
+                        <div className="jp_form_exper_wrapper">
+                          <i className="fa fa-dot-circle-o first_icon"></i>
+                          <select
+                            onChange={(e) => {
+                              setSalary(e.target.value);
+                            }}
+                          >
+                            <option value="">Mức lương</option>
+                            {Salary.map((salary, index) => (
+                              <option key={index + 1} value={index + 1 || ""}>
+                                {salary}
+                              </option>
+                            ))}
+                          </select>
+                          <i className="fa fa-angle-down second_icon"></i>
+                        </div>
                       </div>
                     </div>
                     <div className="col-lg-2  col-md-2 col-sm-12 col-xs-12">
@@ -166,33 +369,6 @@ const JobList = () => {
                             </button>
                           </li>
                         </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="jp_listing_tabs_wrapper">
-                    <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                      <div className="gc_causes_view_tabs_wrapper">
-                        <div className="gc_causes_view_tabs">
-                          <ul className="nav nav-pills">
-                            <li className="active">
-                              <a data-toggle="pill" href="#grid">
-                                <i className="fa fa-th-large"></i>
-                              </a>
-                            </li>
-                            <li>
-                              <a data-toggle="pill" href="#list">
-                                <i className="fa fa-list"></i>
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-                      <div className="gc_causes_search_box_wrapper gc_causes_search_box_wrapper2">
-                        <div className="gc_causes_search_box">
-                          <p>Trang 01 tới 20</p>
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -305,13 +481,14 @@ const JobList = () => {
                             <div className="jp_job_post_main_wrapper_cont jp_job_post_grid_main_wrapper_cont">
                               <div className="jp_job_post_main_wrapper">
                                 <div className="row">
-                                  <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12"
+                                  <div
+                                    className="col-lg-8 col-md-8 col-sm-8 col-xs-12"
                                     onClick={() =>
                                       openInNewTab(`/job/${recommendJob.id}`)
                                     }
                                   >
                                     <div className="jp_job_post_side_img">
-                                    <img
+                                      <img
                                         src={recommendJob.company.avatar_url}
                                         alt="post_img"
                                       />
@@ -344,7 +521,7 @@ const JobList = () => {
                                         </li>
                                         <li>
                                           <i className="fa fa-map-marker"></i>
-                                          &nbsp; {recommendJob.addresss}
+                                          &nbsp; {recommendJob.city.name}
                                         </li>
                                       </ul>
                                     </div>
